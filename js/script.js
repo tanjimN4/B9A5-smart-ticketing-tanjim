4250718be8e47e08689054a30749a8bb32b713f8
tanjimN4 <tanjimahemed7@gmail.com>
2024-02-19 18:47:34 +0600

    new11

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-document.addEventListener('click',handleKeyboardButtonPress )
 
+document.addEventListener('click', handleKeyboardButtonPress);
 
 
 
@@ -9,17 +9,93 @@ function handleKeyboardButtonPress(event) {
     if (event.target.classList.contains('k')) {
         const seat = document.getElementById('seatLeft');
         const seatText = seat.innerText;
-        let currentSeat = parseInt(seatText);
+        const currentSeat = parseInt(seatText);
+
+
+        const seatSelect =document.getElementById('seatSelect')
+        const seatSelectText=seatSelect.innerText;
+        const currentSeatSelect = parseInt(seatSelectText)
+        if(currentSeatSelect < 8){
+            const newSeatSelect =currentSeatSelect +1;
+            seatSelect.innerText=newSeatSelect;
+        }
+
+
         if (currentSeat > 0) {
             const newSeat = currentSeat - 1;
             seat.innerText = newSeat;
+
             event.target.classList.add('bg-[#1DD100]');
             event.target.disabled = true;
+
+            const buttonText = event.target.innerText;
+            const classText = 'Economy';
+            const priceText = '550';
+            const textToAdd = buttonText + ' - ' + classText + ' - BDT ' + priceText;
+
+            handleKeyboardButtonPress2(textToAdd);
+
+            updateTotalPrice(parseInt(priceText));
+
+            
         } else {
             alert('No more seats available!');
         }
     }
 }
 
+function handleKeyboardButtonPress2(textToAdd) {
+    const container = document.getElementById('scp');
+    container.classList.add('grid', 'grid-cols-3', 'text-center');
     
+    const textArray = textToAdd.split(' - ');
+    textArray.forEach(text => {
+        const textElement = document.createElement('div');
+        textElement.textContent = text;
+        container.appendChild(textElement);
+    });
+}
+
+
+
+
+function updateTotalPrice(price) {
+    const totalElement = document.getElementById('total');
+    const currentTotal = parseInt(totalElement.innerText.replace('BDT ', ''));
+    const newTotal = currentTotal + price;
+    totalElement.innerText = 'BDT ' + newTotal;
+}
+
+
+function calculateDiscount() {
+    const totalElement = document.getElementById('total');
+    const grandTotalElement = document.getElementById('grandtotal');
+    const inputValue = document.getElementById('inputFild').value.trim();
+    const discountButton = document.getElementById('apply');
+
+    if (inputValue === 'NEW15') {
+        const currentTotal = parseFloat(totalElement.innerText.replace('BDT',''));
+        const discountAmount = currentTotal * 0.15; 
+        const discountedTotal = currentTotal - discountAmount;
+        grandTotalElement.innerText = 'BDT ' + discountedTotal.toFixed(2);
+        discountButton.disabled = true;
+    }
+    if (inputValue === 'Couple20') {
+        const currentTotal = parseFloat(totalElement.innerText.replace('BDT ', ''));
+        const discountAmount = currentTotal * 0.20;
+        const discountedTotal = currentTotal - discountAmount;
+        grandTotalElement.innerText = 'BDT ' + discountedTotal.toFixed(2);
+        discountButton.disabled = true;
+    }
+
+    
+     else {
+        grandTotalElement.innerText=totalElement.innerText
+    }
+}
+
+function lastCode(){
+
+}
+
 
